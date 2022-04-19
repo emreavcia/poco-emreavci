5fe5d1a5a4b29205be8a73dd0f4fa827da005f4e
emreavcia <avciemrea@gmail.com>
2022-04-19 14:10:36 +0200

    second exercise is done

--- Week04/script.js
+++ Week04/script.js
@@ -1,3 +1,4 @@
+const myBirthYear = 1990;
+const futureYear = 2085;
 
-
-
+console.log("I will be either " + (futureYear - myBirthYear) + " or " + (futureYear - myBirthYear + 1) + " in " + futureYear)
