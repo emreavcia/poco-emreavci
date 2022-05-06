6dbf6ba016c9be61b183f88beb87eafb9e0c1fd7
emreavcia <avciemrea@gmail.com>
2022-05-06 19:34:24 +0200

    the comments are added

--- Week06/script.js
+++ Week06/script.js
@@ -2,6 +2,7 @@ const btn = document.querySelector("button")
 const input = document.querySelector("#todo")
 const ol = document.querySelector("ol")
 
+//initial list
 let todos = [
     {
         id: 1,
@@ -10,11 +11,14 @@ let todos = [
     }
 ]
 
+// function to handle change in the checkboxes
 const handleChange = (e) => {
     todos = todos.map(item => item.id == e.currentTarget.id ? { ...item, isDone: !item.isDone } : item)
+    //to render content when the checkbox are checked
     renderContent(todos)
 }
 
+// function to render the content
 const renderContent = (pArray) => {
 
     content = pArray.map((item, index) => {
@@ -25,14 +29,13 @@ const renderContent = (pArray) => {
         </li>
         `
     }).join("")
-
     ol.innerHTML = content
-
 }
 
+// render the list on load
 renderContent(todos)
 
-
+// event listener for submit button
 btn.addEventListener("click", () => {
 
     const text = input.value
@@ -40,13 +43,17 @@ btn.addEventListener("click", () => {
 
     id = todos.length == 0 ? id = 1 : id = todos[todos.length - 1].id + 1
 
+    // validate if the input value isn't empty  
     if (text) {
         const newItem = {
             id,
             text,
             isDone: false
         }
+        // add new to do to the todo lists
         todos = [...todos, newItem]
+
+        // render the content evertime the new to do added 
         renderContent(todos)
     }
 })
